af9f6142edcba23f50fc3538672d02189f2af392
Milana-Budzanovska <budzanovskamilana@gmail.com>
2025-04-05 23:10:31 +0300

    Update LearningMenu.tsx

--- src/LearningMenu.tsx
+++ src/LearningMenu.tsx
@@ -17,21 +17,21 @@ const LearningMenu = () => {
           className="bg-white rounded-2xl shadow-xl p-6 hover:scale-105 transition transform text-center"
           onClick={() => handleSelect("visual")}
         >
-          <img src="/visual-icon.png" alt="Візуал" className="mx-auto w-20 mb-4" />
+          <img src="/1000043625-removebg-preview.png" alt="Візуал" className="mx-auto w-20 mb-4" />
           <p className="font-bold text-purple-600">Для візуала</p>
         </button>
         <button
           className="bg-white rounded-2xl shadow-xl p-6 hover:scale-105 transition transform text-center"
           onClick={() => handleSelect("audio")}
         >
-          <img src="/audio-icon.png" alt="Аудіал" className="mx-auto w-20 mb-4" />
+          <img src="/IMG_20250307_010159_215.png" alt="Аудіал" className="mx-auto w-20 mb-4" />
           <p className="font-bold text-pink-600">Для аудіала</p>
         </button>
         <button
           className="bg-white rounded-2xl shadow-xl p-6 hover:scale-105 transition transform text-center"
           onClick={() => handleSelect("kinesthetic")}
         >
-          <img src="/touch-icon.png" alt="Кінестетик" className="mx-auto w-20 mb-4" />
+          <img src="/1000043681-fotor-bg-remover-20250312224319.png" alt="Кінестетик" className="mx-auto w-20 mb-4" />
           <p className="font-bold text-yellow-600">Для кінестетика</p>
         </button>
       </div>
