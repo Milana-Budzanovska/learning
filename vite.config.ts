2e46dcc7ea497b6156752bfa3635eff616933951
Milana-Budzanovska <budzanovskamilana@gmail.com>
2025-04-05 16:29:34 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-})
+  assetsInclude: ['**/*.png', '**/*.mp3', '**/*.wav'],
+  server: {
+    port: 5173,
+  },
+  build: {
+    outDir: 'dist',
+  },
+});
+
