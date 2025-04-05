b0934f6910ce15ee23a0691e5544903e5b382126
Milana-Budzanovska <budzanovskamilana@gmail.com>
2025-04-05 17:36:40 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,29 @@
-// src/App.tsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LearningMenu from './LearningMenu';
 import VisualLesson from './VisualLesson';
 import AudioLesson from './AudioLesson';
 import KinestheticLesson from './KinestheticLesson';
+import Assistant from './components/Assistant'; // новий компонент асистента
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<LearningMenu />} />
-        <Route path="/visual" element={<VisualLesson />} />
-        <Route path="/audio" element={<AudioLesson />} />
-        <Route path="/kinesthetic" element={<KinestheticLesson />} />
-      </Routes>
+      <div className="relative">
+        {/* Голосовий асистент */}
+        <Assistant />
+
+        {/* Основні маршрути */}
+        <Routes>
+          <Route path="/" element={<LearningMenu />} />
+          <Route path="/visual" element={<VisualLesson />} />
+          <Route path="/audio" element={<AudioLesson />} />
+          <Route path="/kinesthetic" element={<KinestheticLesson />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
 
 export default App;
+
