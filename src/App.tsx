0f2ca251a66a98f4a219bbb1d167cf40207c8f56
Milana-Budzanovska <budzanovskamilana@gmail.com>
2025-04-05 06:38:31 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
-import React from "react";
-import LessonPage from "./LessonPage";
+// src/App.tsx
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LearningMenu from './LearningMenu';
+import VisualLesson from './VisualLesson';
+import AudioLesson from './AudioLesson';
+import KinestheticLesson from './KinestheticLesson';
 
-const App = () => {
-  return <LessonPage />;
-};
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LearningMenu />} />
+        <Route path="/visual" element={<VisualLesson />} />
+        <Route path="/audio" element={<AudioLesson />} />
+        <Route path="/kinesthetic" element={<KinestheticLesson />} />
+      </Routes>
+    </Router>
+  );
+}
 
 export default App;
