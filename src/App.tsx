7e7d27af25926cb336bcdcc73791502e87762c90
Milana-Budzanovska <budzanovskamilana@gmail.com>
2025-04-05 19:48:28 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,28 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import LearningMenu from './LearningMenu';
 import VisualLesson from './VisualLesson';
 import AudioLesson from './AudioLesson';
 import KinestheticLesson from './KinestheticLesson';
-import Assistant from './components/Assistant'; // новий компонент асистента
+import Assistant from './components/Assistant';
 
 function App() {
   return (
     <Router>
       <div className="relative">
-        {/* Голосовий асистент */}
-        <Assistant />
-
-        {/* Основні маршрути */}
         <Routes>
           <Route path="/" element={<LearningMenu />} />
           <Route path="/visual" element={<VisualLesson />} />
           <Route path="/audio" element={<AudioLesson />} />
           <Route path="/kinesthetic" element={<KinestheticLesson />} />
         </Routes>
+
+        {/* Флік завжди присутній */}
+        <Assistant />
       </div>
     </Router>
   );
 }
 
 export default App;
-
