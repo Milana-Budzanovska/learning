c3b07a4ceb9df55284177d8968a87fd1cd89c740
Milana-Budzanovska <budzanovskamilana@gmail.com>
2025-04-06 19:53:50 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
 import LearningMenu from './LearningMenu';
 import VisualLesson from './VisualLesson';
 import AudioLesson from './AudioLesson';
 import KinestheticLesson from './KinestheticLesson';
-import Assistant from './components/Assistant';
+import NeuroReport from './NeuroReport';
 
 function App() {
   return (
     <Router>
-      <div className="relative">
-        <Routes>
-          <Route path="/" element={<LearningMenu />} />
-          <Route path="/visual" element={<VisualLesson />} />
-          <Route path="/audio" element={<AudioLesson />} />
-          <Route path="/kinesthetic" element={<KinestheticLesson />} />
-        </Routes>
-
-        {/* Флік завжди присутній */}
-        <Assistant />
-      </div>
+      <Routes>
+        <Route path="/" element={<LearningMenu />} />
+        <Route path="/visual" element={<VisualLesson />} />
+        <Route path="/audio" element={<AudioLesson />} />
+        <Route path="/kinesthetic" element={<KinestheticLesson />} />
+        <Route path="/neuro-report" element={<NeuroReport />} />
+      </Routes>
     </Router>
   );
 }
